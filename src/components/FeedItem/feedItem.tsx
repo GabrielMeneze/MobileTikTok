247882d9053b12e21b7290488d224096160a1863
Gabriel Oliveira <65770195+GabrielMeneze@users.noreply.github.com>
2023-08-15 14:55:05 -0300

    right buttons

--- src/components/FeedItem/feedItem.tsx
+++ src/components/FeedItem/feedItem.tsx
@@ -1,6 +1,7 @@
-import { View, StyleSheet, Text, Pressable, Dimensions } from "react-native";
+import { View, StyleSheet, Text, Pressable, Dimensions, Touchable, TouchableOpacity, Platform } from "react-native";
 import { useRef, useState } from "react";
 import { Video } from 'expo-av'
+import { Ionicons } from '@expo/vector-icons'
 
 const styles = StyleSheet.create({
     info: {
@@ -8,21 +9,33 @@ const styles = StyleSheet.create({
         zIndex: 99,
         left: 8,
         padding: 8
-
     },
-    name:{
+    name: {
         color: '#fff',
         fontWeight: 'bold',
         marginBottom: 4,
         textShadowColor: 'rgba(0,0,0,0.60)',
-        textShadowOffset: {width: -1, height: 1.5},
+        textShadowOffset: { width: -1, height: 1.5 },
         textShadowRadius: 8
     },
-    description:{
+    description: {
         color: '#fff',
-        marginRight: 14,
+        marginRight: 24,
         textShadowColor: 'rgba(0,0,0,0.20)',
-        textShadowOffset: {width: -1, height: 1.5},
+        textShadowOffset: { width: -1, height: 1.5 },
+        textShadowRadius: 8
+    },
+    actions: {
+        position: "absolute",
+        zIndex: 99,
+        right: 10,
+        bottom: Platform.OS === 'android' ? 120 : 170,
+        gap: 8 
+    },
+    actionText:{
+        textAlign: 'center',
+        color: '#fff',
+        textShadowOffset: { width: -1, height: 1.5 },
         textShadowRadius: 8
     }
 })
@@ -39,11 +52,28 @@ export function FeedItem({ data }) {
     return (
         <Pressable onPress={handlePlayer}>
             <View
-                style={[styles.info, {bottom: 110}]}
+                style={[styles.info, { bottom: 110 }]}
             >
                 <Text style={styles.name}>{data?.name}</Text>
                 <Text numberOfLines={2} style={styles.description}>{data?.description}</Text>
             </View>
+
+            <View style={styles.actions}>
+                <TouchableOpacity style={styles.actionButton}>
+                    <Ionicons name="heart" size={35} color="#fff"></Ionicons>
+                    <Text style={styles.actionText}>30.3k</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.actionButton}>
+                    <Ionicons name="chatbubble-ellipses" size={35} color="#fff"></Ionicons>
+                    <Text style={styles.actionText}>88</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.actionButton}>
+                    <Ionicons name="bookmark" size={35} color="#fff"></Ionicons>
+                </TouchableOpacity>
+            </View>
+
             <Video
                 ref={video}
                 style={{ width: '100%', height: heightScreen }}
